797070dbe565e33b9089720f94b5d4a429969ae2
Thomas Moffet <pantherman1996@gmail.com>
2017-04-05 07:43:43 +1000

    Made it so that the two motors are operated by the analog triggers, and the console carriage returns for easy reading.

--- RumbleTest/src/Main.cpp
+++ RumbleTest/src/Main.cpp
@@ -10,19 +10,30 @@ int main(int argc, char* argv[]) {
 
 	while (true) {
 		XINPUT_STATE state = GetControllerState(0);
-		std::cout << "Controller state: X = " << state.Gamepad.sThumbLX << ", Y = " << state.Gamepad.sThumbLY << "\n";
+		//std::cout << "Controller state: L = " << (int)state.Gamepad.bLeftTrigger << ", R = " << (int)state.Gamepad.bRightTrigger << "\n";
 
 		XINPUT_VIBRATION vibration;
 		ZeroMemory(&vibration, sizeof(XINPUT_VIBRATION));
 
-		vibration.wLeftMotorSpeed = (uint16_t)state.Gamepad.sThumbLX + 32768;
-		vibration.wRightMotorSpeed = (uint16_t)state.Gamepad.sThumbLY + 32768;
+		XINPUT_BATTERY_INFORMATION battery;
+		ZeroMemory(&battery, sizeof(XINPUT_BATTERY_INFORMATION));
+
+
+		XInputGetBatteryInformation(0, 0, &battery);
+		//std::cout << "Battery type: " << (int)battery.BatteryType << ", Level: " << (int)battery.BatteryLevel << "\n";
+
+		XINPUT_CAPABILITIES capabilities;
+		ZeroMemory(&capabilities, sizeof(XINPUT_CAPABILITIES));
+
+		vibration.wLeftMotorSpeed = (uint16_t)state.Gamepad.bRightTrigger * 256;
+		vibration.wRightMotorSpeed = (uint16_t)state.Gamepad.bLeftTrigger * 256;
+
+		XInputGetCapabilities(0, 0, &capabilities);
+		std::cout << "Vibration: L = " << vibration.wLeftMotorSpeed << ", R = " << vibration.wRightMotorSpeed << "      \r";
 
 		Sleep(25);
 		XInputSetState(0, &vibration);
 	}
-
-	//dwResult = XInputGetBatteryInformation(0, )
 
 	return 0;
 }
@@ -32,7 +43,6 @@ XINPUT_STATE GetControllerState(DWORD user) {
 	XINPUT_STATE state;
 	ZeroMemory(&state, sizeof(XINPUT_STATE));
 
-	// Simply get the state of the controller from XInput.
 	dwResult = XInputGetState(user, &state);
 
 	if (dwResult != ERROR_SUCCESS) {
